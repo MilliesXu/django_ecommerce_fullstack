105f2fd98ad593ee06a6b7cf1d0e25a09052a7fd
MilliesXu <winzchip@gmail.com>
2022-02-06 23:17:07 +0700

    Add order in backend

---
+++ order/serializers.py
@@ -0,0 +1,42 @@
+from rest_framework import serializers
+from .models import Order, OrderItem, ShippingAddress
+from user.serializers import UserSerializer
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
+
+class ShippingAddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ShippingAddress
+        fields = '__all__'
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = serializers.SerializerMethodField(read_only=True)
+    shipping_address = serializers.SerializerMethodField(read_only=True)
+    user = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+    def get_items(self, obj):
+        items = obj.orderitem_set.all()
+        serializer = OrderItemSerializer(items, many=True)
+
+        return serializer.data
+
+    def get_shipping_address(self, obj):
+        try:
+            shipping_address = ShippingAddressSerializer(obj.shippingaddress, many=False)
+        except:
+            shipping_address = False
+        
+        return shipping_address
+
+    def get_user(self, obj):
+        user = UserSerializer(obj.user, many=False)
+
+        return user
+
--- order/views.py
+++ order/views.py
@@ -1,3 +1,62 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Order, OrderItem, ShippingAddress
+from .serializers import OrderSerializer
+from product.models import Product
 
 # Create your views here.
+class OrderView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        user = request.user
+        data = request.data
+
+        order_items = data['order_items']
+
+        if not order_items and len(order_items) == 0:
+             return Response({
+                'detail': 'No order items'
+             }, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            # Create Order
+            order = Order.objects.create(
+                user = user,
+                payment_method = data['payment_method'],
+                tax_price = data['tax_price'],
+                shipping_price = data['shipping_price'],
+                total_price = data['total_price'],
+            )
+
+            # Create Shipping Address
+            shipping = ShippingAddress.objects.create(
+                order = order,
+                address = data['shipping_address']['address'],
+                city = data['shipping_address']['city'],
+                postal_code = data['shipping_address']['postal_code'],
+                country = data['shipping_address']['country'],
+            )
+
+            # Create Order Item
+            for order_item in order_items:
+                product = Product.objects.get(id=order_item['product'])
+
+                item = OrderItem.objects.create(
+                    product = product,
+                    order = order,
+                    name = product.name,
+                    qty = order_item['qty'],
+                    price = order_item['price'],
+                    image = product.image.url,
+                )
+
+                # Update Stock
+                product.countInStock -= item.qty
+                product.save()
+
+        serializer = OrderSerializer(order, many=False)
+        return Response(serializer.data)
